710ea8ef983a3bdf7ecdf4979e97a38ac46c7674
Yvanoff <petruzzi.yvan@gmail.com>
2022-08-27 15:55:35 +0200

    added comments to clarify

--- srcs/equal.hpp
+++ srcs/equal.hpp
@@ -2,6 +2,12 @@
 #define EQUAL_HPP
 
 namespace ft {
+    /*
+     * Simply iterating to check if the elements are equal
+     * within the range given.
+     * False if there is no equality
+     * True if there is equality
+     */
     template <class InputIterator1, class InputIterator2>
     bool equal ( InputIterator1 first1, InputIterator1 last1,
                  InputIterator2 first2 )
@@ -14,6 +20,14 @@ namespace ft {
         }
         return true;
     }
+
+    /*
+     * Adding BinaryPredicate
+     * A Binary Predicate is a Binary Function whose result represents
+     * the truth or falsehood of some condition. A Binary Predicate might,
+     * for example, be a function that takes two arguments and tests whether
+     * they are equal.
+     */
     template <class InputIterator1, class InputIterator2>
     bool equal ( InputIterator1 first1, InputIterator1 last1,
                  InputIterator2 first2, BinaryPredicate pred )
