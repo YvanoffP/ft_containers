0fd0d43b25b818c903534a53df5173825139e00c
Yvanoff <petruzzi.yvan@gmail.com>
2022-08-27 17:16:38 +0200

    added makepair

--- srcs/pair.hpp
+++ srcs/pair.hpp
@@ -20,28 +20,31 @@ namespace ft {
         pair(const pair<A, B> &p) : first(p.first), second(p.second) {}
     };
     template<class _T1, class _T2>
-    bool &operator==(const std::pair<_T1, _T2> &lhs, const std::pair<_T1, _T2> &rhs)
+    bool &operator==(const ft::pair<_T1, _T2> &lhs, const ft::pair<_T1, _T2> &rhs)
     { return lhs.first == rhs.first && lhs.second == rhs.second; }
 
     template<class _T1, class _T2>
-    bool &operator!=(const std::pair<_T1, _T2> &lhs, const std::pair<_T1, _T2> &rhs)
+    bool &operator!=(const ft::pair<_T1, _T2> &lhs, const ft::pair<_T1, _T2> &rhs)
     { return !(lhs == rhs); }
 
     template<class _T1, class _T2>
-    bool &operator<=(const std::pair<_T1, _T2> &lhs, const std::pair<_T1, _T2> &rhs)
+    bool &operator<=(const ft::pair<_T1, _T2> &lhs, const ft::pair<_T1, _T2> &rhs)
     { return lhs <= rhs; }
 
     template<class _T1, class _T2>
-    bool &operator>=(const std::pair<_T1, _T2> &lhs, const std::pair<_T1, _T2> &rhs)
+    bool &operator>=(const ft::pair<_T1, _T2> &lhs, const ft::pair<_T1, _T2> &rhs)
     { return lhs >= rhs; }
 
     template<class _T1, class _T2>
-    bool &operator<(const std::pair<_T1, _T2> &lhs, const std::pair<_T1, _T2> &rhs)
+    bool &operator<(const ft::pair<_T1, _T2> &lhs, const ft::pair<_T1, _T2> &rhs)
     { return lhs.first < rhs.first || lhs.first < rhs.second && lhs.second < rhs.second; }
 
     template<class _T1, class _T2>
-    bool &operator>(const std::pair<_T1, _T2> &lhs, const std::pair<_T1, _T2> &rhs)
+    bool &operator>(const ft::pair<_T1, _T2> &lhs, const ft::pair<_T1, _T2> &rhs)
     { return lhs.first > rhs.first || lhs.first > rhs.second && lhs.second > rhs.second; }
+    
+    ft::pair make_pair(_T1 lhs, _T2 rhs)
+    { return ft::pair<_T1, _T2>(lhs, rhs); }
 }
 
 #endif // PAIR_HPP
